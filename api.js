9dd06c85f530a0676ed0468b47d5c6740530430c
Denis St <haosmark@gmail.com>
2018-10-06 22:12:57 -0400

    added roomId to be able to navigate to rooms. Minor tweaks

--- api.js
+++ api.js
@@ -1,19 +1,19 @@
-import { mockData } from './mockData'
-import { IN_GAME, IN_ROOM, IN_LOBBY, ACTIVE_MATCH, STAGING_ROOM } from './constants'
+import { mockData } from './mockData';
+import { IN_GAME, IN_ROOM, IN_LOBBY, ACTIVE_MATCH, STAGING_ROOM } from './constants';
 
-const url = 'https://info.server.cnc-online.net/'
+const url = 'https://info.server.cnc-online.net/';
 
-let result
-let playerList = {}
-let activeGames = {}
-let stagingRooms = {}
+let result;
+let playerList = {};
+let activeGames = {};
+let stagingRooms = {};
 
-const debugMode = false
+const debugMode = false;
 
 export const fetchDataAsync = async () => {
-  playerList = {}
-  activeGames = {}
-  stagingRooms = {}
+  playerList = {};
+  activeGames = {};
+  stagingRooms = {};
 
   //console.log('fetching data')
   if (debugMode) {
@@ -25,8 +25,8 @@ export const fetchDataAsync = async () => {
 
 
   Object.keys(result.users).map(parsePlayers)
-  result.games.playing.map(parseRooms(ACTIVE_MATCH))
-  result.games.staging.map(parseRooms(STAGING_ROOM))
+  result.games.staging.map(parseRooms(STAGING_ROOM, startingId = 0))
+  result.games.playing.map(parseRooms(ACTIVE_MATCH, startingId = Object.keys(stagingRooms).length + 1))
 
   if (debugMode) {
     new Promise((resolve, reject) => {
@@ -45,20 +45,22 @@ const parsePlayers = (user) => {
     id,
     name,
     room: '',
+    roomId: -1,
     roomPlayerCount: 0,
     status: IN_LOBBY,
     visible: true // used for player list filtering
   }
 }
 
-const parseRooms = roomType => (room, i) => {
+const parseRooms = (roomType, startingId) => (room, i) => {
   const { title, map, pw: password, numObservers: observerCount, numplayers: playerCount, maxRealPlayers: totalSlots, numRealPlayers: claimedSlots } = room
   const players = []
   // decide whether to add the room to a map of active games, or staging rooms
-  let temp = (roomType === ACTIVE_MATCH) ? activeGames : stagingRooms
+  let temp = (roomType === ACTIVE_MATCH) ? activeGames : stagingRooms;
+  let roomId = startingId + i;
 
   temp[title] = {
-    id: i,
+    id: roomId,
     title,
     map,
     password,
@@ -74,8 +76,9 @@ const parseRooms = roomType => (room, i) => {
   room.players.map(player => {
     playerList[player.nickname].status = (roomType === ACTIVE_MATCH) ? IN_GAME : IN_ROOM
     playerList[player.nickname].room = title
+    playerList[player.nickname].roomId = roomId
     playerList[player.nickname].roomPlayerCount = playerCount
-    players.push(player.nickname)
+    players.push({ name: player.nickname, id: player.pid })
   })
 
   temp = null
