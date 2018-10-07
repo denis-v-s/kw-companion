992ca7e46a6f7663d100c371e7d1d6cacb2869ff
Denis St <haosmark@gmail.com>
2018-10-06 22:26:57 -0400

    added mapReducer

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import appReducer from './app_reducer';
 import playerReducer from './player_reducer';
 import roomReducer from './room_reducer';
+import mapReducer from './map_reducer';
 
 export default combineReducers({
   app: appReducer,
   player: playerReducer,
-  room: roomReducer
+  room: roomReducer,
+  map: mapReducer,
 });
